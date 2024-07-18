c2756dcd03adfeab077abfab68fd22d57c767aa9
Carlo Pinciroli <carlo@pinciroli.net>
2024-07-18 11:54:20 -0400

    Added tool to upload archive to Leo robots

---
+++ src/tools/upload_to_leo.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+TARGET=all
+if [[ $# -eq 1 ]]; then
+    TARGET=$1
+fi
+
+# Descend into base folder
+pushd ../../../
+
+# Compress the sources and files
+tar cjvf argos3-leo.tar.bz2 --exclude build argos3-leo
+
+# Copy the archive
+if [[ "x${TARGET}" = "xall" ]]; then
+    for LEO in leo1 leo2 leo3; do
+        scp argos3-leo.tar.bz2 ${LEO}:~/ARGoS
+    done
+else
+    scp argos3-leo.tar.bz2 ${TARGET}:~/ARGoS
+fi
+
+popd
