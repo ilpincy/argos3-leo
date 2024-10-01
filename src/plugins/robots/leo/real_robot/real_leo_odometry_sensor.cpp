cf8f10508d203f83f71d36d6d7fb7e0b4e00f14e
AshayAswale <asaswale@wpi.edu>
2024-10-01 19:24:59 -0400

    Updated odom listen topic to odometry_merged

--- src/plugins/robots/leo/real_robot/real_leo_odometry_sensor.cpp
+++ src/plugins/robots/leo/real_robot/real_leo_odometry_sensor.cpp
@@ -6,7 +6,7 @@
 
 CRealLeoOdometrySensor::CRealLeoOdometrySensor(ros::NodeHandle& c_node_handle) :
    CRealLeoROSDevice(c_node_handle) {
-   m_cSub = c_node_handle.subscribe("/wheel_odom_with_covariance",
+   m_cSub = c_node_handle.subscribe("/odometry_merged",
                                     1,
                                     &CRealLeoOdometrySensor::Update,
                                     this);
